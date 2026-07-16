a2053455122d4884002385c544f5505ad2c01229
ChronoKora <ancientgamekora@gmail.com>
2026-07-16 22:24:28 +0200

    Fix

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const BKMP_DATA_KEY = 'bkmp-data';
 const BKMP_THEME_KEY = 'bkmp-theme';
 
 const BKMP_INCOME_CATEGORIES = [
-  'Karten', 'Bücher', 'Tränke', 'Elytra', 'Raketen', 'Werkzeug', 'Schmiede Vorlagen', 'Netherite', 'Custom'
+  'Karten', 'Bücher', 'Tränke', 'Elytra', 'Raketen', 'Werkzeug', 'Schmiede Vorlagen', 'Netherite', 'Selbst Verdient', 'Custom'
 ];
 
 const BKMP_CARD_SALE_PRICE = 150000;
